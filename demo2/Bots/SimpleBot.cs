3fc6fd63712ee632952d29ffd688623bf4605e6f
James Mann <james.mann@bcs.org>
2018-07-18 12:56:06 +0100

    added demo2

---
+++ demo2/Bots/SimpleBot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo2.Bots
+{
+    public class SimpleBot : IBot
+    {
+        public async Task OnTurn(ITurnContext turnContext)
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                var userSaid = turnContext.Activity.Text;
+                await turnContext.SendActivity($"You said {userSaid}");
+            }
+        }
+    }
+}
---
+++ demo2/Middleware/SimpleMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo2.Middleware
+{
+    public class SimpleMiddleware : IMiddleware
+    {
+        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            await context.SendActivity("from middleware");
+
+            await next();
+        }
+    }
+}
