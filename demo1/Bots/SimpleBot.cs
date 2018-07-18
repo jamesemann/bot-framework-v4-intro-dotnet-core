7233744ec51e72542bf66a0a336196fac74cd494
James Mann <james.mann@bcs.org>
2018-07-18 03:12:19 +0100

    first commit

---
+++ demo1/Bots/SimpleBot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo1.Bots
+{
+    public class SimpleBot : IBot
+    {
+        public async Task OnTurn(ITurnContext turnContext)
+        {
+            if (turnContext.Activity.Type is ActivityTypes.Message)
+            {
+                string input = turnContext.Activity.Text;
+                await turnContext.SendActivity($"SimpleBot: {input}");
+            }
+        }
+    }
+}
