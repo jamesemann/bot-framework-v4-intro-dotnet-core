5e8219ea95027be3f188ae31f840ed4f96cd0c94
James Mann <james.mann@bcs.org>
2018-07-20 21:30:40 +0100

    added state demo

---
+++ demo8statemiddleware/Bots/StateBot.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading.Tasks;
+
+namespace demo8statemiddleware.Bots
+{
+    public class StateBot : IBot
+    {
+        public async Task OnTurn(ITurnContext turnContext)
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                await turnContext.SendActivity($"You said {turnContext.Activity.Text}");
+            }
+        }
+    }
+}
---
+++ demo8statemiddleware/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using demo7dialogs.Bots;
+using demo8statemiddleware.Bots;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Startup
+{
+    // Inject the IHostingEnvironment into constructor
+    public Startup(IHostingEnvironment env)
+    {
+        // Set the root path
+        ContentRootPath = env.ContentRootPath;
+    }
+
+    // Track the root path so that it can be used to setup the app configuration
+    public string ContentRootPath { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Set up the service configuration
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(ContentRootPath)
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables();
+        var configuration = builder.Build();
+        services.AddSingleton(configuration);
+
+        // Add your SimpleBot to your application
+        services.AddBot<StateBot>(options =>
+        {
+            options.CredentialProvider = new ConfigurationCredentialProvider(configuration);
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        app.UseStaticFiles();
+
+        // Tell your application to use Bot Framework
+        app.UseBotFramework();
+    }
+}
