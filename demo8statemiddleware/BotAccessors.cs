b7431495cc0b414aaa5f84960799742e4b248da2
James Mann <james.mann@bcs.org>
2018-09-29 08:58:52 +0100

    updated state bot

---
+++ demo8statemiddleware/BotAccessors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using demo8statemiddleware.Bots;
+using Microsoft.Bot.Builder;
+
+namespace demo8statemiddleware
+{
+    public class BotAccessors
+    {
+        public BotAccessors(ConversationState conversationState)
+        {
+            ConversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+        }
+
+        public static string DemoStateName { get; } = $"{nameof(BotAccessors)}.DemoStateAccessor";
+
+        public IStatePropertyAccessor<DemoState> DemoStateAccessor { get; set; }
+        public ConversationState ConversationState { get; }
+    }
+}
--- demo8statemiddleware/Bots/MyBot.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Demo.Bots
-{
-    public class MyBot
-    {
-    }
-}
